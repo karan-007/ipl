0707b1df1d8d2cc91403d1e32af59bb32b29a9e8
Karan <karanchouhan159@gmail.com>
2020-04-02 20:57:51 +0530

    new commit

---
+++ ipl.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+// Read all CSV and computation here 
+
+// Build and dump data.json and extraruns.json will be in root directory and data.json will be in Publick directoruy
+
+
+// ---
+// index.json
+
+// Connect static server and read extraruns.json DOMMatrixReadOnly. 
