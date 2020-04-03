ed5e7568d67364cab1bd3449cebf0f8024b0c168
Karan <karanchouhan159@gmail.com>
2020-04-03 12:39:00 +0530

    new with /economy?season=changes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var someObject = require('./extraRuns.json')
+
 var express = require('express');
 var app = express();
 const bodyParser = require('body-parser');
@@ -6,18 +8,13 @@ app.use(express.static("./public"));
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-let port= process.env.PORT || 8080
-app.listen(port,() =>{
-  console.log("server is running"+port)
+app.get('/economy', (req, res) => {
+  const season = req.query.season;
+  const result = someObject.extraRuns[season];
+  res.send(result)
 })
 
-
-// app.get('/economy', (req, res) => {
-//   const season = req.query.season;
-//   const result = extraRuns(allMatches,allDeliveries,season);
-//   res.send(result)
-// })
-
-
-
-
+const port= process.env.PORT || 8080;
+app.listen(port,() =>{
+  console.log("server is running")
+})
--- public/displayExtraRuns.js
+++ public/displayExtraRuns.js
@@ -1,58 +1,46 @@
-let year = 0
 function showData() {
-  year = document.getElementById("seasons1").value;
-  fetch("./extraRuns.json")
-    .then(resp => resp.json())
-    .then(visualizeData)
-    }
-
-function visualizeData(data){
-    visualizeExtraRuns(data.extraRuns)
-}
-
-      function visualizeExtraRuns(extraRuns) {
-          data={}
-        const seriesData = [];
-        for(let season in extraRuns){
-            if(season === year){
-                data=extraRuns[season]
-            }
-        }
-        for(let team in data){
-            seriesData.push([team,data[team]]);
-        }
-        console.log(data)
-        Highcharts.chart("container2", {
-          chart: {
-            type: "column"
-          },
-          title: {
-            text: `Extra Runs Conceded By Each Team in ${year}`
-          },
-          subtitle: {
-            text:
-              'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data">IPL Dataset</a>'
-          },
-          xAxis: {
-            title: {
-              text: "Teams"
+    const season = document.getElementById("seasons1").value;
+    fetch("/economy?season=" + season)
+      .then(resp => resp.json())
+      .then(response => {
+          visualizeExtraRuns(response);
+        function visualizeExtraRuns(response) {
+          const seriesData = [];
+          for (let team in response) {
+            seriesData.push([team, response[team]]);
+          }
+          Highcharts.chart("container2", {
+            chart: {
+              type: "column"
             },
-            type: "category"
-          },
-          yAxis: {
-            min: 0,
             title: {
-              text: "Extra Runs"
-            }
-          },
-          series: [
-            {
-              name: "Extra Runs",
-              data: seriesData
-            }
-          ]
-        });
-      }
-    
-    
-
+              text: `Extra Runs Conceded By Each Team in ${season}`
+            },
+            subtitle: {
+              text:
+                'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data">IPL Dataset</a>'
+            },
+            xAxis: {
+              title: {
+                text: "Teams"
+              },
+              type: "category"
+            },
+            yAxis: {
+              min: 0,
+              title: {
+                text: "Extra Runs"
+              }
+            },
+            series: [
+              {
+                name: "Extra Runs",
+                data: seriesData
+              }
+            ]
+          });
+        }
+      }).catch(err => {
+  console.error('Error: ', err);
+});
+}
